87c820be553382ff2e7a857e370a0dd05d12d470
menaahmed22 <menaahmedh22@gmail.com>
2026-06-16 15:44:23 +0300

    commit changes

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -14,31 +14,43 @@ location = streamlit_geolocation()
 if location and location['latitude'] is not None:
     st.write("Latitude:", location["latitude"])
     st.write("Longitude:", location["longitude"])
+    latitude=location['latitude']
+    longitude=location['longitude'] 
 else:
     st.write("Waiting for location...")
 
+
+
 st.write("Click the button below to choose the type of needed places:")
 options = ["school","university","travel_agency","supermarket" ,"mosque", "church","resturant", "bank","atm","bakery","beauty_salon","book_store","bicycle_store",
-            "cafe","car_wash","clothing_store","shopping_mall","hospital","dentist","doctor","electronics_store"
-            "gas_station","gym","laundry","pharmacy","parking","park","museum","library"]
+                "cafe","car_wash","clothing_store","shopping_mall","hospital","dentist","doctor","electronics_store",
+                "gas_station","gym","laundry","pharmacy","parking","park","museum","library"]
 search_type = st.pills("search_type", options , selection_mode="single")
-st.write("Your outing option is  :", search_type)
+if search_type is not None:
+    search_type =search_type   
+    st.write("Your outing option is  :", search_type)
+else:
+    st.write("Waiting for search_type...")    
 
 
 distance = st.slider("How far you want to go",0, 10, 1)
-st.write("Distance in KM :", distance)
 
-count_of_results = st.number_input(
-    "Insert a number of max needed places", value=None, placeholder="Type a number..."
-)
-st.write("The current number is ", count_of_results)
+if distance is not None:
+    distance_km=distance
+    st.write("Distance in KM :", distance)
+else:
+    st.write("Waiting for distance...")  
 
 
-latitude=location['latitude']
-longitude=location['longitude']   
-distance_km=distance
-search_type =search_type
-count_of_results = count_of_results
+count_of_results = st.number_input("Insert a number of max needed places", value=None, placeholder="Type a number...")
+
+if count_of_results is not None:
+       count_of_results = count_of_results
+    st.write("The current number is ", count_of_results)
+else:
+    st.write("Waiting for count_of_results...")
+
+  
 
 payload = {
     "latitude": latitude,
